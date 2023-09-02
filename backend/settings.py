70ccecd2b3c8adde8ee314cf8340d55a261c28cf
Gurshaan Kooner <gurshaankooner@Gurshaans-MacBook-Pro.local>
2023-09-02 16:50:10 -0400

    change env module

--- backend/settings.py
+++ backend/settings.py
@@ -1,18 +1,18 @@
 import os
-# import environ
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# env = environ.Env()
-# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') #env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY') #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shopEcomm',
-        'USER': os.environ.get('DB_USER'), #env('DB_USER')
-        'PASSWORD': os.environ.get('DB_PASS'), #env('DB_PASS')
+        'USER': env('DB_USER'), #os.environ.get('DB_USER'),
+        'PASSWORD': env('DB_PASS'), #os.environ.get('DB_PASS'), 
         'HOST': 'shopecomm-id.cpbiagnf2lkk.us-east-2.rds.amazonaws.com',
         'POST': '5432'
     }
@@ -147,8 +147,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') #env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') #env('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID =  env('AWS_ACCESS_KEY_ID') #os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY') #os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'shopecomm-bucket'
 AWS_QUERYSTRING_AUTH = False
 
